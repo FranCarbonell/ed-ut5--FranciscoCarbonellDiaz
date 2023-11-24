a742e553f3ee2f2043e3af42649ddc270d40c261
Carlos Ruiz Perez <carruiper2@alu.edu.gva.es>
2023-11-24 18:52:35 +0100

    Segunda versión del proyecto colaborativo. He añadido otro saludo.

--- src/Saludo.java
+++ src/Saludo.java
@@ -3,5 +3,7 @@ import javax.swing.*;
 public class Saludo {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hola Carlos");
+
+        JOptionPane.showMessageDialog(null, "¡Hola, Fran!");
     }
 }
